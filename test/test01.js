a2b76ef23f997fade6241742f55b4acd67209522
sergiioss <caos13es@hotmail.com>
2022-07-09 10:20:06 +0200

    test01

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,18 @@
 function geekshubs(index) {
-    return "";
+    let result = "";
+
+    for(let i=1; i <= index; i++){
+        if(i % 5 == 0 && i % 3 == 0){
+            result += "GeeksHubs\n"
+        }else if(i % 3 == 0) {
+            result += "Geeks\n"
+        }else if(i % 5 == 0){
+            result += "Hubs\n"
+        }else{
+            result += i.toString() + "\n"
+        }
+    }
+
+    return result;
 }
 module.exports = geekshubs;
